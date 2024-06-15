ad400dd4d9c35917504c066b7f67b1fd6be0bb35
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2024-06-16 00:18:10 +0530

    Solve Q1 & Q2

--- Arrays/array.js
+++ Arrays/array.js
@@ -1,1 +1,20 @@
-'use strict'
+"use strict";
+
+//1. Print odd nos from 1 to N
+/*
+let N = 10;
+for (let i = 1; i <= N; i++) {
+  i = i + 2;
+  console.log(i);
+}
+*/
+
+//2. Loop for bowling all balls in an over
+/*
+let count = 0;
+const balls = 6;
+for (let i = 1; i <= balls; i++) {
+  count += 1;
+}
+console.log(count);
+*/
