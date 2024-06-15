9b66d3a96e00eccbe45a8d6bcf868abcf328afee
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2024-06-16 01:13:08 +0530

    Solve Q5

--- Arrays/array.js
+++ Arrays/array.js
@@ -44,3 +44,24 @@ if (N === 0) {
   }
 }
 */
+
+//5. Given an integer N, print sum of all digits
+/*
+const N = 12111992;
+let number = N;
+let sum = 0;
+if (number === 0) {
+  console.log(0);
+} else if (number < 0) {
+  number = number * -1;
+  for (; number > 0; number = Math.floor(number / 10)) {
+    let last_digit = number % 10;
+    sum += last_digit;
+  }
+}
+for (; number > 0; number = Math.floor(number / 10)) {
+  let last_digit = number % 10;
+  sum += last_digit;
+}
+console.log(sum);
+*/
