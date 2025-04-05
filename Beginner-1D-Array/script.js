ccb9d9525cfec00a5c0b3f1147ed7b3f427f41f0
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-05 09:00:31 +0530

    1D-Array

---
+++ Beginner-1D-Array/script.js
@@ -0,0 +1,26 @@
+"use strict"
+//1. Find the max of an array
+/*
+let arr = [5,2,3,4,1];
+
+let maxArr=arr[0];
+for(let i=0; i<arr.length; i++){
+  if(arr[i]>maxArr){
+    maxArr = arr[i];
+  }
+}
+
+console.log(maxArr);
+*/
+//2. Find the min of an array
+/*
+let arr = [5,3,2,1,0];
+
+let minArr = arr[0];
+for(let i=0; i<arr.length; i++){
+  if(arr[i]<minArr){
+    minArr = arr[i];
+  }
+}
+console.log(minArr);
+*/
