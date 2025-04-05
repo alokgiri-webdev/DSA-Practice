07063c6675b520e29734b8cb6cff7c88a3670550
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-05 18:29:17 +0530

    DSA-Assignments

--- Maths/Maths.js
+++ Maths/Maths.js
@@ -123,3 +123,136 @@ if(count>2){
 }
 }
 */
+
+// ASSIGHNMENT QUESTIONS
+/*
+1. Problem Description -----------------------------------------------------------
+
+Write a program that asks the user to input a number T, indicating the number of test cases.
+Then, for each test case, ask for input a number N and print the first and last digits of N.
+
+Problem Constraints
+
+1 <= T <= 1000
+
+0 <= N <= 100000000
+
+Input Format:
+First line is T which means number of test cases.
+Each next T lines contain an integer N.
+
+Output Format
+
+T lines each containing two space separated integers representing first and last digits of the input integer.
+
+Example Input
+
+Input 1:
+
+2
+5
+1001
+Input 2:
+
+2
+10023
+1589
+
+
+Example Output
+
+Output 1:
+
+5 5
+1 1
+Output 2:
+
+1 3
+1 9
+
+*/
+//Solution
+/*
+const firstLastDigit = function(N){
+  let lastDigit = N%10;
+  let firstDigit = N;
+  while(firstDigit>=10){
+    firstDigit = Math.floor(firstDigit/10);
+  }
+  return `${firstDigit} & ${lastDigit}`;
+}
+
+let T = parseInt(prompt(`No. of Test cases`));
+for(let t=0; t<T; t++){
+  let N = parseInt(prompt(`Value of N`));
+  console.log(firstLastDigit(N));
+}
+*/
+
+/*
+2. Problem Description -----------------------------------------------
+
+Take an integer N as input and print the count of its factors.
+The factor of a number is the number that divides it perfectly leaving no remainder.
+
+Example: 1, 2, 3, and 6 are factors of 6
+
+Problem Constraints
+
+1 <= N <= 300
+
+Input Format
+
+The first and only line of input contains a single integer N.
+
+Output Format
+
+Print the count of factors of N.
+
+Example Input
+
+Input 1:
+ 5
+Input 2:
+ 10
+
+Example Output
+
+Output 1:
+2
+Output 2:
+4
+
+Example Explanation
+
+Explanation 1:
+ Factors of 5 are 1 and 5.
+Explanation 2:
+ Factors of 10 are 1, 2, 5 and 10.
+*/
+
+/* Solution
+const countOfFactors = function(N){
+  let count = 2;
+  if(N===0){
+    count = 0;
+  }else if(N===1){
+    count = 1;
+  }
+  if(N<0){
+    N = N*(-1);
+  }
+  for(let i=2; i<N; i++){
+    if(N%i===0){
+    count++;
+    }
+  }
+  return count;
+}
+
+let T = parseInt(prompt('No. of Test cases'));
+for(let t=0; t<T; t++){
+  let N = parseInt(prompt('Enter value of N'));
+  console.log(countOfFactors(N));
+}
+*/
