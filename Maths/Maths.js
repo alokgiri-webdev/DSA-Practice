9c1a2dd524ab282d79b793cd9e554c26969bba34
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-05 19:45:25 +0530

    DSA-Assignments

--- Maths/Maths.js
+++ Maths/Maths.js
@@ -256,3 +256,148 @@ for(let t=0; t<T; t++){
   console.log(countOfFactors(N));
 }
 */
+
+/*
+3. Problem Description -----------------------------------------------
+
+Take an integer A as input, you have to tell whether it is a prime number or not.
+
+A prime number is a natural number greater than 1 which is divisible only by 1 and itself.
+
+
+Problem Constraints
+
+1 <= A <= 106
+
+
+
+Input Format
+
+First and only line of the input contains a single integer A.
+
+
+
+Output Format
+
+Print YES if A is a prime, else print NO.
+
+
+
+Example Input
+
+Input 1:
+
+ 3 
+Input 2:
+
+ 4 
+
+
+Example Output
+
+Output 1:
+
+ YES 
+Output 2:
+
+ NO 
+*/
+/*Solution
+const checkPrime = function(N){
+  if(N===1 || N===0 || N<0){
+    return `NO`;
+  }else{
+    for(let i=2; i<=Math.sqrt(N); i++){
+      if(N%i===0){
+        return `NO`;
+      }
+    }
+    return `YES`;
+  }
+}
+
+let T = parseInt(prompt('No. of test cases'));
+for(let t=0; t<T; t++){
+  let N = parseInt(prompt('Enter value of N'));
+  console.log(checkPrime(N));
+}
+*/
+
+/*
+Given the Number of Test Cases as T,
+For each test case, take an integer N as input, you have to tell whether it is a perfect number or not.
+
+A perfect number is a positive integer that is equal to the sum of its proper positive divisors (excluding the number itself). 
+A positive proper divisor divides a number without leaving any remainder.
+
+
+Problem Constraints
+
+1 <= T <= 10
+1 <= N <= 106
+
+
+Input Format
+
+The first line of the input contains a single integer T. 
+Each of the next T lines contains a single integer N.
+
+
+Output Format
+
+For each testcase, print YES if the given integer is perfect, else print NO, in a separate line
+
+
+Example Input
+
+Input 1:
+2
+4
+6
+Input 2:
+1
+3
+
+
+Example Output
+
+Output 1:
+NO
+YES 
+Output 2:
+NO
+
+
+Example Explanation
+
+Explanation 1:
+For the first test case A = 4, the answer is "NO" as sum of its proper divisors = 1 + 2 = 3, is not equal to 4.
+For the second test case A = 6, the answer is "YES" as sum of its proper divisors = 1 + 2 + 3 = 6, is equal to 6.
+Explanation 2:
+For the first test case A = 3, the answer is "NO" as sum of its proper divisors = 1, is not equal to 3.
+*/
+
+/* Solution
+const checkPerfectNo = function(N){
+  if(N===0){
+    return `${N} is not a perfect no.`;
+  }
+  let sum = 0;
+  for(let i=1; i<N/2; i++){ //Optimised
+    if(N%i===0){
+      sum += i;
+    }
+  }
+  if(sum === N){
+    return `${N} is a perfect no.`;
+  }else{
+    return `${N} is not a perfect no.`;
+  }
+}
+let T = parseInt(prompt('No. of Test cases'));
+for(let t=0; t<T; t++){
+  let N = parseInt(prompt('Enter value of N'));
+  console.log(checkPerfectNo(N));
+}
+*/
+
