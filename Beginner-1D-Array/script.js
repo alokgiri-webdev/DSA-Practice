ee5033e5971578e4bc14e146b0f00efe710651e8
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-05 09:24:32 +0530

    1D-Array

--- Beginner-1D-Array/script.js
+++ Beginner-1D-Array/script.js
@@ -24,3 +24,18 @@ for(let i=0; i<arr.length; i++){
 }
 console.log(minArr);
 */
+
+//3. Write a function which returns an array containing the max and min of the array
+/*
+const maxMinArray = function(arr){
+  let maxArr = arr[0];
+  let minArr = arr[0];
+  for(let i=1; i<arr.length; i++){
+    if(arr[i]>maxArr) maxArr = arr[i];
+    if(arr[i]<minArr) minArr = arr[i]; 
+    }
+    return [maxArr,minArr];
+  }
+let arr = [5,3,7,2,9,8]
+console.log(maxMinArray(arr));
+*/
