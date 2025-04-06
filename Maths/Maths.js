8b69900fd1e0f93fd03dd7e628861e3aff82316a
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-06 14:04:53 +0530

    DSA-Assignments

--- Maths/Maths.js
+++ Maths/Maths.js
@@ -576,3 +576,52 @@ const findHCF = function(a, b){
 }
 console.log(findHCF(intOne,intTwo));
 */
+
+/*
+2. Problem Description
+You take a number of test cases, denoted by T as input.For each test case, you should take integers N as input. Your task is to calculate and print the sum of the digits of the given number N.
+
+Problem Constraints
+
+1 <= T <= 1000
+0 <= N <= 100000000
+Input Format
+
+The first line is T which means the total number of test cases.
+Each of the next T lines contain an integer N.
+. Output Format
+
+T lines each containing one integer representing the sum of the digits of the input integer.
+Example Input
+
+Input 1:
+2
+5
+1001
+
+Input 2:
+2
+123
+1589
+Example Output
+
+Output 1:
+5
+2
+
+Output 2:
+6
+23
+*/
+
+let number = parseInt(prompt('Enter the no.'));
+
+const sumOfDigits = function(a){
+  let sum = 0;
+    for(; a>0; a=Math.floor(a/10)){
+      let digit = a%10;
+      sum += digit;
+    }
+  return sum;
+}
+console.log(sumOfDigits(number));
