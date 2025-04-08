cf21eaaac9a07452f601cac3d3e1395e50171431
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-08 08:47:13 +0530

    DSA-Assignments

--- Patterns/script.js
+++ Patterns/script.js
@@ -888,11 +888,9 @@ Output 2:
 */
 /*
 const printStarsPattern = (N) => {
-for(let row=N; row>0; row--){
   let stars = '';
-  for(let col=1; col<=row; col++){
-    stars += '*';
-  }
+for(let row=N; row>0; row--){
+  stars = '*'.repeat(row);
   console.log(stars);
 }
 }
@@ -963,5 +961,65 @@ const printStarsPattern = (N) => {
       console.log("*"+spaces+"*");
   }
 }
+printStarsPattern(4);
+*/
+
+/*
+4. Problem Description
+
+Take an integer N as input, print the corresponding pattern for N.
+For example if N = 5 then pattern will be like:
+
+*****
+_****
+__***
+___**
+____*
+Note : Here '_' represents space for explanation purpose only. You have to print space in your code.
+
+Problem Constraints
+
+1 <= N <= 100
+
+Input Format
+
+First and only line of input contains a single integer N.
+
+Output Format
+
+Output the pattern corresponding to the given N.
+
+Example Input
+
+Input 1:
+
+ 2
+Input 2:
+
+ 3
+Example Output
+
+Output 1:
+
+**
+_*
+Output 2:
+
+ 
+ ***
+ _**
+ __*
+Note : Here '_' represents space for explanation purpose only. You have to print space in your code.
+*/
+/*
+const printStarsPattern = (N) => {
+  let pattern = "";
+  for(let row = 1; row<=N; row++){
+  let spaces = ' '.repeat(row-1);
+  let stars = '*'.repeat(N-row+1);
+  pattern = spaces+stars;
+  console.log(pattern);
+  }
+  }
 printStarsPattern(4);
 */
