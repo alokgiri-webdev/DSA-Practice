97425747fefc04d384d3b85f4995bdf9f65d7684
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-09 11:22:18 +0530

    DSA-Assignments

--- Patterns/script.js
+++ Patterns/script.js
@@ -1482,3 +1482,79 @@ const printPattern = (N) => {
 }
 printPattern(3);
 */
+
+/*
+4. Problem Description
+
+Take an integer N, print the corresponding Full Pyramid pattern for N.
+
+For example if N = 5 then pattern will be like:
+
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+NOTE: There should be exactly one extra space after each * for each row.
+
+
+Problem Constraints
+
+2 <= N <= 100
+
+
+
+Input Format
+
+First and only line of input contains a single integer N.
+
+
+
+Output Format
+
+Output the Full Pyramid pattern corresponding to the given N.
+
+
+
+Example Input
+
+Input 1:
+
+ 2
+Input 2:
+
+ 3
+Input 3:
+
+ 4
+
+
+Example Output
+
+Output 1:
+
+  * 
+ * * 
+Output 2:
+
+   * 
+  * * 
+ * * * 
+Output 3:
+
+    * 
+   * * 
+  * * * 
+ * * * * 
+
+*/
+/*
+const printStars = (N) => {
+  for(let row=1; row<=N; row++){
+    let spaces = ' '.repeat(N-row);
+    let stars = '* '.repeat(row);
+    console.log(spaces+stars);
+  }
+}
+printStars(3);
+*/
