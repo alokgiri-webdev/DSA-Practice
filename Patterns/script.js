318e2b70a183ac77ebc1bd0e3f42ce91862d947f
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-10 20:49:32 +0530

    DSA-Assignments

--- Patterns/script.js
+++ Patterns/script.js
@@ -1701,3 +1701,67 @@ const printNumPattern = (N) => {
 
 printNumPattern(3);
 */
+
+/*
+3. Problem Description
+
+Take an integer N as input, print the corresponding Half Diamond pattern with 2*N - 1 rows.
+
+For example if N = 5 then pattern will be like:
+
+* 
+**
+***
+****
+*****
+****
+***
+**
+*
+NOTE: There should be no spaces after any * .
+
+
+Problem Constraints
+
+2 <= N <= 100
+
+
+
+Input Format
+
+First and only line of input contains a single integer N.
+
+
+
+Output Format
+
+Output the Half Diamond pattern corresponding to the given N.
+
+
+
+Example Input
+
+Input 1:
+
+ 2
+Input 2:
+
+ 3
+
+
+Example Output
+
+Output 1:
+
+ * 
+ **
+ *
+Output 2:
+
+ *
+ **
+ ***
+ **
+ *
+ 
+*/
