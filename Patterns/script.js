0cf84268d1c20cb5a128a46d8cfb6d658ea85c4b
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-13 09:42:53 +0530

    Advance Patterns revision

--- Patterns/script.js
+++ Patterns/script.js
@@ -1777,3 +1777,54 @@ const printPattern = (N) => {
 }
 printPattern(3);
 */
+
+//Patterns to solve for Practice
+/*
+1. For example if N = 5 then pattern will be like:
+
+0 0 0 0 5 0 0 0 0 
+0 0 0 4 8 12 0 0 0 
+0 0 3 6 9 12 15 0 0
+0 2 4 6 8 10 12 14 0
+1 2 3 4 5 6 7 8 9 
+*/
+
+/*
+2. For example if N = 5 then pattern will be like:
+
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+ * * * *
+  * * *
+   * *
+    *
+*/
+
+/*
+3. For example if N = 5 then pattern will be like:
+
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+
+*/
+
+/*
+4. For example if N = 5 then pattern will be like:
+
+0 0 0 0 1 0 0 0 0 
+0 0 0 2 3 2 0 0 0 
+0 0 3 4 5 4 3 0 0
+0 4 5 6 7 6 5 4 0
+5 6 7 8 9 8 7 6 5 
+
+*/
