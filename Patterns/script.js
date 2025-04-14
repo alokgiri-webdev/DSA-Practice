c38d9b6a6ebb40e69b73330bf19684f83f3826cb
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-14 23:20:23 +0530

    Advance Maths revision

--- Patterns/script.js
+++ Patterns/script.js
@@ -1828,3 +1828,13 @@ printPattern(3);
 5 6 7 8 9 8 7 6 5 
 
 */
+
+/*
+4. For example if N = 5 then pattern will be like:
+
+0 0 0 0 1 0 0 0 0 
+0 0 0 2 3 2 0 0 0 
+0 0 3 4 5 4 3 0 0
+0 4 5 6 7 6 5 4 0
+5 6 7 8 9 8 7 6 5 
+*/
