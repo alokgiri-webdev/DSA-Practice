97c04c46e0496d68c7026884839d44b1d5fc07d7
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-15 21:30:40 +0530

    Beginer 1-D Arrays

--- Beginner-1D-Array/script.js
+++ Beginner-1D-Array/script.js
@@ -102,3 +102,47 @@ This question requires you to read a list of integers from input using the map()
 Since this concept will be covered later in the module, we encourage you to do some independent research and write your code accordingly.
 You may also refer to the following article for further guidance: link
 */
+
+/*
+2. Problem Description
+
+Take input an array A of size N and write a program to print maximum and minimum elements of the input. The only line of the input would contain a single integer N that represents the length of the array followed by the N elements of the input array A.
+
+Problem constraints
+
+1 <= N <= 1000
+
+1 <= A <= 1000
+
+
+Input Format
+
+The first line contains a single integer N representing the length of the array A followed by N elements of the array A.
+
+
+Output Format
+
+A single line output containing two space-separated integers.
+The first integer is the maximum value of the array.
+The second integer is the minimum value of the array. 
+
+There is **no space** after the minimum value in the output format. 
+There is only a **single space** between the maximum and minimum value.
+
+
+Example Input
+
+Input 1:
+5 1 2 3 4 5
+Input 2:
+4 10 50 40 80
+
+Example Output
+
+Output 1:
+5 1
+Output 2:
+80 10
+Note: There is no space after the minimum value in the output format.
+There is only a single space between the maximum and minimum value.
+*/
