4a963333fda691e6d318f468519b0b25ff364e84
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-15 21:31:12 +0530

    Beginer 1-D Arrays

--- Beginner-1D-Array/script.js
+++ Beginner-1D-Array/script.js
@@ -146,3 +146,72 @@ Output 2:
 Note: There is no space after the minimum value in the output format.
 There is only a single space between the maximum and minimum value.
 */
+
+/*
+3. Problem Description
+
+Write a program to print sum of elements of the input array A of size N.
+
+Note: User is supposed to write a program from scratch to add the elements of the input array and print the result.
+
+
+Problem Constraints
+
+
+1 <= N <= 1000
+
+1 <= A <= 1000
+
+
+
+
+Input Format
+
+- The first integer N denotes the number of integers in the array.
+- The next N integers denotes the element of the array A.
+
+
+
+Output Format
+
+A single integer containing sum of elements from the input array.
+
+
+
+
+Example Input
+
+
+Input 1:
+
+
+5 1 2 3 4 5
+Input 2:
+
+4 10 50 40 80
+
+
+
+Example Output
+
+Output 1:
+
+15
+Output 2:
+
+180
+
+
+
+
+Example Explanation
+
+Input-1:
+N = 5 , A = [1, 2, 3, 4, 5]
+here the first integer is the size of A and the remaining elements are [1, 2, 3, 4, 5]
+which all sum to 15 
+Input-2:
+N = 4 , A = [10, 50, 40, 80]
+here the first integer is the size of A and the remaining elements are [10, 50, 40, 80]
+which all sum to 180
+*/
