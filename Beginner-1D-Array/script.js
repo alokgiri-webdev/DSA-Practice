4c248d6183d5a43de41b4a94d78d84aabb87727d
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-22 03:26:31 +0530

    Beginner 1-D Array

--- Beginner-1D-Array/script.js
+++ Beginner-1D-Array/script.js
@@ -324,3 +324,47 @@ This question requires you to read multiple numbers from input. The first number
 Since this concept will be covered later in the module, we encourage you to do some independent research and write your code accordingly.
 You may also refer to the following article for further guidance: link
 */
+
+//ADDITIONAL PROBLEMS-----------------------------------------------------
+/*
+1. Problem Description
+
+Write a program to print all negative numbers from input array A of size N. Take integer N and N elements of the array as input from user.
+
+Problem Constraints
+
+1 <= N <= 1000  
+-1000 <= A <= 1000
+Input Format
+
+A single line representing N followed by N integers of the array A
+Output Format
+
+A single line containing elements from the input array which are negative in the same order.
+Note: Keep a space character (' ') at the end of the line.
+Example Input
+
+Input 1:
+
+5 1 -5 2 -8 -4
+Input 2:
+
+4 -1 0 -8 -1
+Example Output
+
+Output 1:
+
+-5 -8 -4 
+Output 2:
+
+-1 -8 -1 
+Example explanation:
+
+Output 1:
+
+all the negative elements of [5 1 -5 2 -8 -4] are [-5 -8 -4]
+
+Output 2:
+
+all the negative elements of [4 -1 0 -8 -1] are [-1 -8 -1]
+*/
