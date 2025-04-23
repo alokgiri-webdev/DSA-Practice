496bab0a28f96a762d1fc91ed3a808fa1474d6e3
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-23 18:08:49 +0530

    Beginner 1-D Arraylist

--- Beginner-1D-ArrayList/script.js
+++ Beginner-1D-ArrayList/script.js
@@ -531,3 +531,64 @@ Expected Output
 Provide sample input and click run to see the correct output for the provided input. Use this to improve your problem understanding and test edge cases
 */
 //Solution:
+
+/*
+5. Problem Description
+
+You are given an integer array A, you have to return an integer array of same size whose ith element is the frequency count of A[i] in array A .
+
+Note:
+
+When comparing two elements of the ArrayList(java), make sure to use .equals() method for accurate comparison.
+Eg : To compare ith index with jth index use A.get(i).equals(A.get(j)).
+
+
+
+Problem Constraints
+
+1 <= len(A) <= 1000
+
+
+1 <= A[i] <= 100
+
+
+
+
+Input Format
+
+First argument is an array of integers representing array A.
+
+
+
+Output Format
+
+You have to return an array of integers as per the question.
+
+
+
+Example Input
+
+Input 1:
+
+A = [1, 2, 5, 1, 5, 1 ]
+
+
+Example Output
+
+Output 1:
+
+[3, 1, 2, 3, 2, 3]
+
+
+Example Explanation
+
+Explanation 1:
+
+Clearly, In the given array we have 1 three times, 2 one time and 5 two times.
+
+
+
+Expected Output
+Provide sample input and click run to see the correct output for the provided input. Use this to improve your problem understanding and test edge cases
+*/
+//Solution:
