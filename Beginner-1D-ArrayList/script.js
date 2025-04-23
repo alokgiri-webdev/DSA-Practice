1db25a8a38a9308be173945da1a9c4c2b9080f8b
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-23 18:02:42 +0530

    Beginner 1-D Arraylist

--- Beginner-1D-ArrayList/script.js
+++ Beginner-1D-ArrayList/script.js
@@ -130,3 +130,113 @@ Explanation 2:
 105 is divisible by (5 and 7).
 
 */
+//Solution:
+
+/*
+3. Problem Description
+
+Given an integer array A. Find and return the frequency of number B in array A.
+
+
+Problem Constraints
+
+1 <= |A| <= 105
+1 <= A[i] <= 109
+1 <= B <= 109
+
+
+Input Format
+
+The first argument is an integer array A.
+The second argument is an integer B.
+
+
+Output Format
+
+Return the frequency of B in the array A.
+
+
+Example Input
+
+Input 1:-
+A = [1, 1, 2, 4, 2]
+B = 2
+Input 2:-
+A = [4, 2, 3, 4, 4]
+B = 4
+
+
+Example Output
+
+Output 1:-
+2
+Output 2:-
+3
+
+
+Example Explanation
+
+Explanation 1:-
+There are two occurences of 2 in array A.
+Explanation 2:-
+There are three occurences of 4 in array A.
+
+Expected Output
+Provide sample input and click run to see the correct output for the provided input. Use this to improve your problem understanding and test edge cases
+
+*/
+//Solution:
+
+/*
+4. Problem Description
+
+Given an array A, check if it is sorted in non-decreasing order or not.
+
+Non-decreasing order means that the numbers in a sequence can stay the same or increase, but they cannot decrease.
+
+
+Problem Constraints
+
+1 <= Ai <= 109
+1 <= |A| <= 105
+
+
+Input Format
+
+The first and only argument contains an integer array A.
+
+
+Output Format
+
+Return 1 if array is sorted else return 0.
+
+
+Example Input
+
+Input 1:
+ A = [1, 2, 2] 
+Input 2:
+ A = [1, 2, 1] 
+
+
+Example Output
+
+Output 1:
+ 1
+Output 2:
+ 0
+
+
+Example Explanation
+
+Explanation 1:
+We can see that the array elements are in non-decreasing order.
+Explanation 2:
+Second element is greater than the third element thus array is not in ascending order, hence not sorted.
+
+
+
+Expected Output
+Provide sample input and click run to see the correct output for the provided input. Use this to improve your problem understanding and test edge cases
+*/
+//Solution:
