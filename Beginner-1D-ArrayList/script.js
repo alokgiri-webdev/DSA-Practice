41d15034053d78731fac52e28d7132dfc4a0df4e
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-25 06:17:26 +0530

    Beginner 2D- Arrays

--- Beginner-1D-ArrayList/script.js
+++ Beginner-1D-ArrayList/script.js
@@ -620,3 +620,76 @@ Expected Output
 Provide sample input and click run to see the correct output for the provided input. Use this to improve your problem understanding and test edge cases
 */
 //Solution:
+
+
+// 
+/*
+5. Problem Description
+
+Given an Array of integers A, every element in it is repeated twice except one, find that unique element.
+
+
+Problem Constraints
+
+1<= Length of array A <= 103
+0<= Ai <=1000
+
+
+Input Format
+
+First argument A is the given array.
+
+
+Output Format
+
+Return an Integer X, such that X occurs once in the A.
+
+
+Example Input
+
+Input 1:
+
+A = [1, 4, 3, 5, 2, 3, 5, 1, 4]
+
+Input 2:
+
+A = [9, 10, 7, 10, 9, 1, 5, 1, 5]
+
+Example Output
+
+Output 1:
+
+2
+Output 2:
+
+7
+
+Example Explanation
+
+In Example 1:
+All the elements except for 2 occurs twice in the array. Hence 2 is the unique element.
+
+
+
+Expected Output
+Provide sample input and click run to see the correct output for the provided input. Use this to improve your problem understanding and test edge cases
+
+*/
+//Solution:
+/*
+const findUnique = (arr)=>{
+  for(let i=0; i<arr.length; i++){
+    let count = 0;
+    for(let j=0; j<arr.length; j++){
+      if(arr[i]===arr[j]){
+        count++;
+      }
+    }
+    if(count === 1){
+      return arr[i];
+    }
+  }
+  return -1;
+}
+console.log(findUnique([9, 10, 10, 9, 1, 5, 1, 5])); 
+*/
