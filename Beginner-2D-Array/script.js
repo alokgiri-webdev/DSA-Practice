a349df85fbb080e387d79e5222414adf945ae3ee
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-25 06:18:39 +0530

    Beginner 2D- Arrays

---
+++ Beginner-2D-Array/script.js
@@ -0,0 +1,152 @@
+"use strict"
+//ASSIGNMENT PROBLEMS---------------------------------------------------------------
+/*
+1. Problem Description
+
+
+Write a program to input an integer N and a N*N matrix Mat from user and print the matrix in wave form (column wise)
+
+See example for clarifications regarding wave print.
+
+Note: Ensure there is a space character (' ') at the end of the line.
+
+
+Problem Constraints
+
+
+1 <= N <= 103
+
+0 <= Mat[i][j] <= 109
+
+
+
+
+Input Format
+
+
+First line is an integer N
+
+Next N lines contain N space separated integers representing the matrix Mat
+
+
+
+
+Output Format
+
+
+A single line containing N*N integers of matrix Mat in wave form (column wise)
+
+
+
+
+Example Input
+
+
+Input 1:
+
+
+3
+4 1 2 
+7 4 4 
+3 7 4 
+Input 2:
+
+
+2
+1 2
+3 4
+
+
+
+Example Output
+
+
+Output 1:
+
+
+4 7 3 7 4 1 2 4 4 
+Output 2:
+
+1 3 4 2
+
+
+
+
+Example Explanation
+
+For Input 1:
+We will first iterate the 1st column from top to bottom and print the elements, 
+then we will iterate the 2nd column from botton to top and print the elements,
+then we will iterate the 3rd column from top to bottom and print the elements.
+For Input 2:
+We will first iterate the 1st column from top to bottom and print the elements, 
+then we will iterate the 2nd column from bottom to top and print the elements.
+*/
+
+/*
+2. Problem Description
+
+Given an N x N matrix, print the elements of the matrix in a wave form row-wise. For the first row, traverse from left to right, for the second row, traverse from right to left, and continue this alternating pattern for the remaining rows.
+
+
+Problem Constraints
+
+1 <= N <= 103
+
+0 <= Mat[i][j] <= 109
+
+
+
+Input Format
+
+First line is an integer N
+
+Next N lines contain N space separated integers representing the matrix Mat
+
+
+
+
+Output Format
+
+A single line containing N*N integers of matrix Mat in wave form (row wise)
+
+Note: Ensure there is a space character (' ') at the end of the line.
+
+
+Example Input
+
+Input 1:
+
+3
+4 1 2
+7 4 4 
+3 7 4
+Input 2:
+
+2
+1 2
+3 4
+
+
+Example Output
+
+Output 1:
+
+4 1 2 4 4 7 3 7 4
+Output 2:
+
+1 2 4 3
+
+
+Example Explanation
+
+For Input 1:
+We will first iterate the 1st row from left to right and print the elements, 
+then we will iterate the 2nd row from right to left and print the elements,
+then we will iterate the 3rd row from left to right and print the elements.
+This looks like a wave.
+For Input 2:
+We will first iterate the 1st row from left to right and print the elements, 
+then we will iterate the 2nd row from right to left and print the elements.
+This looks like a wave.
+*/
