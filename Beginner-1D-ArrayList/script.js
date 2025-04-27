51e02d739c61b2bdfbf22ac908d23a550fe5f0a4
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-27 18:33:38 +0530

    Beginner 2D- Arrays

--- Beginner-1D-ArrayList/script.js
+++ Beginner-1D-ArrayList/script.js
@@ -712,6 +712,7 @@ arr = [1,2,3,5,6,7];
 O/P
 = 4
 */
+/*
 
 const findMissingElement = (arr)=>{
   //Looping over the arr
@@ -725,10 +726,91 @@ const findMissingElement = (arr)=>{
   }
 }
 console.log(findMissingElement([1,2,3,5,6,7]));
-
+*/
 /*
 i=0     diff= 1   skips
 i=1     diff=1    skips
 i=2     diff=2    3+1 = 4
 
+*/
+/*
+Problem Description
+
+You are given an integer array A and an integer B.
+
+You are required to return the count of pairs having sum equal to B.
+
+NOTE: pair (i,j) and (j,i) are same.
+
+
+
+Problem Constraints
+
+1 <= A.size() <= 10000
+
+1 <= A[i] <= 10000
+
+1 <= B <= 10000
+
+
+
+Input Format
+
+First argument is an integer array A.
+
+Seond argument is an integer B.
+
+
+
+Output Format
+
+You have to return an integer representing count of required pairs.
+
+
+
+Example Input
+
+Input 1:
+
+A = [1,2,3,2,1]
+B = 5
+Input 2:
+
+A = [1,1,1]
+B = 2
+
+
+Example Output
+
+Output 1:
+
+2
+Output 2:
+
+3
+
+
+Example Explanation
+
+Explanation 1:
+
+ A[1]+A[2] = A[2]+A[3] = 5
+Explanation 2:
+
+ A[0]+A[1] = A[0]+A[2] = A[1]+A[2] = 2
+
+*/
+
+/*
+const findPair = (arr,k)=>{
+  //Looping over the array
+  let countPair = 0;
+  for(let i=0; i<arr.length-1; i++){
+  if(arr[i]+arr[i+1] === k){
+  countPair++;
+  }
+  }
+  return countPair;
+  }
+  console.log(findPair([1,2,3,2,1],5));
 */
