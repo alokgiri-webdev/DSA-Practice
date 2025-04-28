c7786467194f762059726985d53e19fae9e04627
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-28 23:47:06 +0530

    Beginner 1D- Arraylists

--- Beginner-1D-ArrayList/script.js
+++ Beginner-1D-ArrayList/script.js
@@ -893,3 +893,104 @@ Output 2:
 False
 */
 //Solution:
+
+/*
+4. Problem Description
+
+You are provided with an integer array A. Return another array B of size same as that of A such that B[i] = A[i]3 for 1 <= i <= |A| (1 based indexing).
+
+
+Problem Constraints
+
+1 <= |A| <= 106
+1 <= A[i] <= 106
+
+
+Input Format
+
+The first and only argument is an integer array A.
+
+
+Output Format
+
+Return array B.
+
+
+Example Input
+
+Input 1 :
+A=[2, 6, 8, 1]
+Input 2 :
+A=[5]
+
+
+Example Output
+
+Output 1 :
+[8, 216, 512, 1]
+Output 2 :
+[125]
+
+
+Example Explanation
+
+Example 1 : Cubes of numbers 2, 6, 8 and 1 are 8, 216, 512 and 1 respectively.
+
+Example 2 : Cubes of number 5 is 125.
+*/
+//Solution:
+
+/*
+5. Problem Description
+
+You are given an integer array A, you have to return an integer array of same size whose ith element is the frequency count of A[i] in array A .
+
+Note:
+
+When comparing two elements of the ArrayList(java), make sure to use .equals() method for accurate comparison.
+Eg : To compare ith index with jth index use A.get(i).equals(A.get(j)).
+
+
+
+Problem Constraints
+
+1 <= len(A) <= 1000
+
+
+1 <= A[i] <= 100
+
+
+
+
+Input Format
+
+First argument is an array of integers representing array A.
+
+
+
+Output Format
+
+You have to return an array of integers as per the question.
+
+
+
+Example Input
+
+Input 1:
+
+A = [1, 2, 5, 1, 5, 1 ]
+
+
+Example Output
+
+Output 1:
+
+[3, 1, 2, 3, 2, 3]
+
+
+Example Explanation
+
+Explanation 1:
+
+Clearly, In the given array we have 1 three times, 2 one time and 5 two times.
+*/
