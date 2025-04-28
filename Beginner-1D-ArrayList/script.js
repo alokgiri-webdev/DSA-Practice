9b123f909d4d9c0bb6bd8a6127ce04e2852fa3a8
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-04-28 23:39:33 +0530

    Beginner 1D- Arraylists

--- Beginner-1D-ArrayList/script.js
+++ Beginner-1D-ArrayList/script.js
@@ -733,8 +733,41 @@ i=1     diff=1    skips
 i=2     diff=2    3+1 = 4
 
 */
+
+//ADDITIONAL PROBLEMS-----------------------------------------------------------------
 /*
-Problem Description
+1. Problem Description:
+
+Write a program that returns the product of all elements present in the array.
+
+Note: The list with elements is already passed as an argument to the function. User don't need to take any input. Just perform the task on the passed arguments and return the required result.
+
+Constraints:
+
+1 <= |A| <= 100
+1 <= A <= 100
+Note: It is guaranteed that the resultant product will be <= 1015
+Input Format:
+
+An integer array **A** as the function argument.
+Output Format:
+
+Product of elements in integer format
+Sample Input:
+
+A = [7, 9, 2, 51]
+Sample Output:
+
+6426
+Sample explanation:
+
+The product of all the elements is 7 * 9 * 2 * 51 = 6426 is returned.
+*/
+//Solution:
+
+
+/*
+2. Problem Description
 
 You are given an integer array A and an integer B.
 
