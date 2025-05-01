a2bda0ecab0e34136eef2378cce142d93e109272
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-05-01 21:48:37 +0530

    Length of String

---
+++ Beginner-Strings/script.js
@@ -0,0 +1,49 @@
+'use strict'
+//ASSIGNMENT------------------------------------------------------------------------
+/*
+1. Problem Description
+
+Write a program to input an integer T and then each of T lines will have a string (S).
+
+You have to print T lines each containing length of input string.
+
+
+
+Problem Constraints
+
+1 <= T <= 100
+
+1 <= S.size() <= 1000
+
+
+
+Input Format
+
+First line is T which means number of test cases.
+
+Each next T lines contain a string S.
+
+
+
+Output Format
+
+T lines each containing an integer representing length of the input string.
+
+
+
+Example Input
+
+Input 1:
+
+2
+scaler
+interviewbit
+
+
+Example Output
+
+Output 1:
+
+6
+12
+*/
