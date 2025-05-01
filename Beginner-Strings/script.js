848fcd76487eab8a8def0652b4bca7ad00a765b5
alokgiri-webdev <alokgiri.web.dev@gmail.com>
2025-05-01 21:52:18 +0530

    last Occurance

--- Beginner-Strings/script.js
+++ Beginner-Strings/script.js
@@ -201,3 +201,64 @@ Explanation 2:
  Character 'd' is not present in "abc".
 */
 
+/*
+4. Problem Description
+
+You are given a character string A, having length N and an integer ASCII code B.
+
+You have to tell the rightmost occurrence of the character having ASCII code equal to B, in A or report that it does not exist.
+
+
+
+
+                                                image
+
+
+
+
+
+Problem Constraints
+
+1 <= N <= 105
+
+A[i] ∈ ['a'-'z']
+
+97 <= B <= 122
+
+
+
+Input Format
+
+First argument is a character string A.
+
+Second argument is an integer B.
+
+
+
+Output Format
+
+If there exists an occurrence, return the rightmost index(0 - based), else return -1.
+
+
+
+Example Input
+
+Input 1:
+
+ A = "aabbcc"
+ B = 98 
+Input 2:
+
+ A = "abc"
+ B = 100
+
+
+Example Output
+
+Output 1:
+
+ 3 
+Output 2:
+
+ -1 
+*/
